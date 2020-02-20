d6e6e0f37208debec4cecff4f7da11d47a0be66f
fangjinuo <fs1194361820@163.com>
2020-02-20 15:58:40 +0800

    + supply redis key properties

---
+++ redisclient-core/src/main/java/com/jn/redisclient/key/RedisKeyProperties.java
@@ -0,0 +1,22 @@
+package com.jn.redisclient.key;
+
+public class RedisKeyProperties {
+    private String prefix = RedisKeyBuilder.PREFIX_DEFAULT;
+    private String separation = RedisKeyBuilder.SEPARATOR_DEFAULT;
+
+    public String getPrefix() {
+        return prefix;
+    }
+
+    public void setPrefix(String prefix) {
+        this.prefix = prefix;
+    }
+
+    public String getSeparation() {
+        return separation;
+    }
+
+    public void setSeparation(String separation) {
+        this.separation = separation;
+    }
+}
